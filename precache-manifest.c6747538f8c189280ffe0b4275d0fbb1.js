7e08eb609e05ff8956f051e8da994548bec97aea
claire froelich <claire.froelich@gmail.com>
2020-05-07 20:06:55 -0400

    Updates

---
+++ precache-manifest.c6747538f8c189280ffe0b4275d0fbb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705e1764f16afbef6c0de2e4580d9740",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "ac39bad3aff021915841",
+    "url": "/airbnb-clone/static/css/2.75edc52f.chunk.css"
+  },
+  {
+    "revision": "97f2d6d6c9379a1f72fd",
+    "url": "/airbnb-clone/static/css/main.2b06426c.chunk.css"
+  },
+  {
+    "revision": "ac39bad3aff021915841",
+    "url": "/airbnb-clone/static/js/2.b559179f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/airbnb-clone/static/js/2.b559179f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f2d6d6c9379a1f72fd",
+    "url": "/airbnb-clone/static/js/main.2428f967.chunk.js"
+  },
+  {
+    "revision": "c5806aea30438e4b55ca",
+    "url": "/airbnb-clone/static/js/runtime-main.80c1009c.js"
+  }
+]);
